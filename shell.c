2b6c04e58b0e7d14b4bb1a3f5e94eaf5911f8531
Rick Battagline <rbattagline@gmail.com>
2019-05-08 18:06:40 -0600

    added stdio.h

---
+++ shell.c
@@ -0,0 +1,25 @@
+#include <emscripten.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    printf("Hello World\n");
+    EM_ASM( InitWrappers() );
+    printf("Initialization Complete\n");
+}
+
+void test() {
+    printf("button test\n");
+}
+
+void int_test( int num ) {
+    printf("int test=%d\n", num);
+}
+
+void float_test( float num ) {
+    printf("float test=%f\n", num);
+}
+
+void string_test( char* str ) {
+    printf("string test=%s\n", str);
+}
